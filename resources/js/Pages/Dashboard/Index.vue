f6065ebf437ea46b312004ecba6035b84503cb63
pszemo <pszemo.koziniak@gmail.com>
2026-02-09 23:58:00 +0100

    fix logowanie

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <Head title="Do zrobienia" />
-    <Historia v-if="historia" :historia="historia"/>
-    <hr class="my-5">
+    <Historia v-if="historia" :historia="historia" />
+    <hr class="my-5" />
     <h1 class="mb-8 text-3xl font-bold">Do zrobienia</h1>
-    <search-filter-simple v-model="form.search" class="mr-4 mb-3 w-full max-w-md" @reset="reset"></search-filter-simple>
+    <search-filter-simple v-model="form.search" class="mr-4 mb-3 w-full max-w-md" @reset="reset" />
     <div class="grid grid-cols-5 gap-4">
       <div class="border-2 border-green-500">
         <p class="text-center w-full p-2 text-indigo-600">Zapytania</p>
         <div v-for="item in zapytanias" :key="item.id" class="hover:bg-gray-100 focus-within:bg-gray-100 border-2 rounded p-2 mb-2">
           <Link class="" :href="`/zapytania/${item.id}/edit`">
             <p v-if="item.wznowienie===2" class="p-1 text-red-600">Wznowienie</p>
-            <p class="p-1">{{item.id_zapyt}}</p>
-            <p class="p-1">{{item.client ? item.client.nazwa : 'Brak klienta'}}</p>
-            <p class="p-1">{{item.nazwa_projektu}}</p>
-            <p class="p-1">{{item.data_zlozenia}}</p>
-            <p v-if="item.opracowuje" class="p-1 text-red-600">{{item.opracowuje.last_name}} {{item.opracowuje.first_name}}</p>
+            <p class="p-1">{{ item.id_zapyt }}</p>
+            <p class="p-1">{{ item.client ? item.client.nazwa : 'Brak klienta' }}</p>
+            <p class="p-1">{{ item.nazwa_projektu }}</p>
+            <p class="p-1">{{ item.data_zlozenia }}</p>
+            <p v-if="item.opracowuje" class="p-1 text-red-600">{{ item.opracowuje.last_name }} {{ item.opracowuje.first_name }}</p>
             <p v-else class="p-1 text-gray-400">Nieprzypisane</p>
           </Link>
         </div>
@@ -24,10 +24,10 @@
         <p class="text-center w-full p-2 text-indigo-600">Oferty</p>
         <div v-for="item in ofertas.data" :key="item.id" class="hover:bg-gray-100 focus-within:bg-gray-100 border-2 rounded p-2 mb-2">
           <Link class="" :href="`/oferta/${item.id}/edit`">
-            <p class="p-1">{{item.zapytania ? item.zapytania.nazwa_projektu : 'Brak projektu'}}</p>
-            <p class="p-1">{{item.client ? item.client.nazwa : 'Brak klienta'}}</p>
-            <p class="p-1">{{item.data_kontakt}}</p>
-            <p v-if="item.user" class="p-1 text-red-600">{{item.user.last_name}} {{item.user.first_name}}</p>
+            <p class="p-1">{{ item.zapytania ? item.zapytania.nazwa_projektu : 'Brak projektu' }}</p>
+            <p class="p-1">{{ item.client ? item.client.nazwa : 'Brak klienta' }}</p>
+            <p class="p-1">{{ item.data_kontakt }}</p>
+            <p v-if="item.user" class="p-1 text-red-600">{{ item.user.last_name }} {{ item.user.first_name }}</p>
             <p v-else class="p-1 text-gray-400">Brak użytkownika</p>
           </Link>
         </div>
@@ -36,11 +36,11 @@
         <p class="text-center w-full p-2 text-indigo-600">Klienci kontakty</p>
         <div v-for="item in kontakts" :key="item.id" class="hover:bg-gray-100 focus-within:bg-gray-100 border-2 rounded p-2 mb-2">
           <Link class="" :href="`/kontakt/${item.id}/edit`">
-            <p class="p-1">{{item.client ? item.client.nazwa : 'Brak klienta'}}</p>
-            <p v-if="item.kontaktperson" class="p-1">{{item.kontaktperson.last_name}} {{item.kontaktperson.first_name}}</p>
-            <p class="p-1">{{item.subject}}</p>
-            <p class="p-1">{{item.call_time}}</p>
-            <p v-if="item.user" class="p-1 text-red-600">{{item.user.last_name}} {{item.user.first_name}}</p>
+            <p class="p-1">{{ item.client ? item.client.nazwa : 'Brak klienta' }}</p>
+            <p v-if="item.kontaktperson" class="p-1">{{ item.kontaktperson.last_name }} {{ item.kontaktperson.first_name }}</p>
+            <p class="p-1">{{ item.subject }}</p>
+            <p class="p-1">{{ item.call_time }}</p>
+            <p v-if="item.user" class="p-1 text-red-600">{{ item.user.last_name }} {{ item.user.first_name }}</p>
           </Link>
         </div>
       </div>
@@ -48,10 +48,10 @@
         <p class="text-center w-full p-2 text-indigo-600">Przyszłe projekty</p>
         <div v-for="item in futureProjects" :key="item.id" class="hover:bg-gray-100 focus-within:bg-gray-100 border-2 rounded p-2 mb-2">
           <Link class="" :href="`/futureproject/${item.id}/edit`">
-            <p class="p-1">{{item.nazwa}}</p>
-            <p class="p-1">{{item.client ? item.client.nazwa : 'Brak klienta'}}</p>
-            <p class="p-1">{{item.data_kontakt}}</p>
-            <p v-if="item.user" class="p-1 text-red-600">{{item.user.last_name}} {{item.user.first_name}}</p>
+            <p class="p-1">{{ item.nazwa }}</p>
+            <p class="p-1">{{ item.client ? item.client.nazwa : 'Brak klienta' }}</p>
+            <p class="p-1">{{ item.data_kontakt }}</p>
+            <p v-if="item.user" class="p-1 text-red-600">{{ item.user.last_name }} {{ item.user.first_name }}</p>
           </Link>
         </div>
       </div>
@@ -59,9 +59,9 @@
         <p class="text-center w-full p-2 text-indigo-600">Zadania</p>
         <div v-for="item in zadania" :key="item.id" class="hover:bg-gray-100 focus-within:bg-gray-100 border-2 rounded p-2 mb-2">
           <Link class="" :href="`/zadania/${item.id}/edit`">
-            <p class="p-1">{{item.subject}}</p>
-            <p class="p-1">{{item.deadline}}</p>
-            <p v-if="item.users" class="p-1 text-red-600">{{item.users.last_name}} {{item.users.first_name}}</p>
+            <p class="p-1">{{ item.subject }}</p>
+            <p class="p-1">{{ item.deadline }}</p>
+            <p v-if="item.users" class="p-1 text-red-600">{{ item.users.last_name }} {{ item.users.first_name }}</p>
           </Link>
         </div>
       </div>
@@ -73,10 +73,10 @@
 import { Head, Link } from '@inertiajs/inertia-vue3'
 import Layout from '@/Shared/Layout'
 import Historia from '@/Pages/ActivityLog/Index.vue'
-import SearchFilterSimple from "@/Shared/SearchFilterSimple.vue";
-import throttle from "lodash/throttle";
-import pickBy from "lodash/pickBy";
-import mapValues from "lodash/mapValues";
+import SearchFilterSimple from '@/Shared/SearchFilterSimple.vue'
+import throttle from 'lodash/throttle'
+import pickBy from 'lodash/pickBy'
+import mapValues from 'lodash/mapValues'
 
 export default {
   components: {
